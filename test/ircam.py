7fb1decdfc79311665e7781f6f6d9d3f107facde
Francis James Franklin <fjf@alinameridon.com>
2025-02-28 10:23:17 +0000

    Add command-line arguments to ircam test script and restructure.

--- test/ircam.py
+++ test/ircam.py
@@ -1,15 +1,49 @@
 import sys
 import time
 import serial
+import argparse
 
 from datetime import datetime
 from queue import Queue
 
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from matplotlib.colors import ListedColormap
-from matplotlib.collections import PatchCollection
+
+parser = argparse.ArgumentParser(description="Plotter/Logger for teensy-4 / MLX90640")
+
+parser.add_argument('--device',  help='Specify serial device.',   default='/dev/ttyACM0', type=str)
+parser.add_argument('--prefix',  help='Folder or prefix for log files.',      default='', type=str)
+parser.add_argument('--logger',  help='Log received data.',                   action='store_true')
+parser.add_argument('--show',    help='Show camera view and plot histogram.', action='store_true')
+parser.add_argument('--shift',   help='Shift temperature before plotting.',   default=0.0, type=float)
+parser.add_argument('--service', help='Retry device until available.',        action='store_true')
+
+args = parser.parse_args()
+
+bRunCam = args.logger or args.show
+if not bRunCam:
+    print("ircam: please specify either --logger or --show")
+    sys.exit(2)
+
+while True:
+    try:
+        teensy = serial.Serial(args.device)
+    except:
+        if not args.service:
+            print("ircam: unable to open serial device: " + args.device)
+            sys.exit(1)
+        time.sleep(1) # try again in a bit
+        continue
+    break
+
+if args.logger:
+    filename = args.prefix + datetime.now().strftime("IRCam-%Y%m%d-%H%M%S.csv")
+    try:
+        csv = open(filename, "w")
+    except:
+        print("ircam: unable to open file for writing: " + filename)
+        sys.exit(1)
+else:
+    csv = None
 
 color_ranges = [
     [ 150, (255,255,  0), 216, (255,245,158) ],
@@ -37,72 +71,26 @@ def map_temperatures_to_colors(temperature_list):
         rgblist.append(rgb)
     return rgblist
 
-Nrow = 24
-Ncol = 32
-
-patch = []
-for row in range(Nrow):
-    for col in range(Ncol):
-        patch.append(patches.Rectangle((col,row), 1, 1))
-
-T_lo = -40
-T_hi = 216
-Tlist = np.linspace(T_lo, T_hi, Nrow * Ncol)
-T_mat = Tlist.reshape((Nrow,Ncol))
-
-Nbin = 16
-bins = np.linspace(T_lo, T_hi, Nbin + 1)
-binc = map_temperatures_to_colors((bins[:-1] + bins[1:])/2)
-
-color = map_temperatures_to_colors(Tlist)
-
-cmap = ListedColormap(color)
-coll = PatchCollection(patch, cmap=cmap)
-coll.set_array(np.arange(len(patch)))
-
-def update_figure():
-    Tlist = T_mat.flatten()
-    color = map_temperatures_to_colors(Tlist)
-    coll.cmap = ListedColormap(color)
-    n, _ = np.histogram(Tlist, bins)
-    for count, rect in zip(n, H.patches):
-        rect.set_height(count)
-    plt.pause(0.1)
-
-plt.rcParams['figure.figsize'] = [12, 4]
-fig, (axl, axr) = plt.subplots(1, 2)
-axl.add_collection(coll)
-_, _, H = axr.hist(Tlist, bins, lw=1, ec="grey")
-
-for rgb, rect in zip(binc, H.patches):
-    rect.set(fc=rgb)
-
-axl.set_xlim([0,Ncol])
-axl.set_ylim([0,Nrow])
-axr.set_ylim(top=100)
-
-bRunCam = True
 def on_close(event):
     global bRunCam
     bRunCam = False
-    print('Closed Figure!')
 
-fig.canvas.mpl_connect('close_event', on_close)
-plt.show(block=False)
-plt.pause(1) # Give the plot time to sort itself out
-
-filename = datetime.now().strftime("IRCam-%Y%m%d-%H%M%S.csv")
-csv = open(filename, "w")
-
-teensy = serial.Serial("/dev/ttyACM0")
-
-teensy.write(b';rate 2;auto on;')
+def update_figure():
+    if args.show:
+        Tlist = T_mat.flatten()
+        color = map_temperatures_to_colors(Tlist)
+        coll.cmap = ListedColormap(color)
+        n, _ = np.histogram(Tlist, bins)
+        for count, rect in zip(n, H.patches):
+            rect.set_height(count)
+        plt.pause(0.1)
 
 def csv_write_row(row, temperatures):
-    csv.write("{r}".format(r=row))
-    for t in temperatures:
-        csv.write(",{t:.2f}".format(t=t))
-    csv.write("\n")
+    if csv is not None:
+        csv.write("{r}".format(r=row))
+        for t in temperatures:
+            csv.write(",{t:.2f}".format(t=t))
+        csv.write("\n")
 
 def b64_decypher(c):
     is_b64 = True
@@ -123,7 +111,54 @@ def b64_decypher(c):
 
 def set_pixel_temperature(row, col, T): # range is -40 .. 216 degC
     global T_mat
-    T_mat[row,col] = T + 10
+    T_mat[row,col] = T + args.shift # shift is a hack for test purposes
+
+Nrow = 24
+Ncol = 32
+T_lo = -40
+T_hi = 216
+Tlist = np.linspace(T_lo, T_hi, Nrow * Ncol)
+T_mat = Tlist.reshape((Nrow,Ncol))
+
+if args.show:
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    from matplotlib.colors import ListedColormap
+    from matplotlib.collections import PatchCollection
+
+    patch = []
+    for row in range(Nrow):
+        for col in range(Ncol):
+            patch.append(patches.Rectangle((col,row), 1, 1))
+
+    Nbin = 32
+    bins = np.linspace(T_lo, T_hi, Nbin + 1)
+    binc = map_temperatures_to_colors((bins[:-1] + bins[1:])/2)
+
+    color = map_temperatures_to_colors(Tlist)
+
+    cmap = ListedColormap(color)
+    coll = PatchCollection(patch, cmap=cmap)
+    coll.set_array(np.arange(len(patch)))
+
+    plt.rcParams['figure.figsize'] = [12, 4]
+    fig, (axl, axr) = plt.subplots(1, 2)
+    axl.add_collection(coll)
+    _, _, H = axr.hist(Tlist, bins, lw=1, ec="grey")
+
+    for rgb, rect in zip(binc, H.patches):
+        rect.set(fc=rgb)
+
+    axl.set_xlim([0,Ncol])
+    axl.set_ylim([0,Nrow])
+    axr.set_ylim(top=100)
+
+    fig.canvas.mpl_connect('close_event', on_close)
+    plt.show(block=False)
+    plt.pause(1) # Give the plot time to sort itself out
+
+# We're ready to go. Set the camera's refresh rate to 2Hz and switch to auto reporting
+teensy.write(b';rate 2;auto on;')
 
 Q = Queue(maxsize=0)
 temperatures = None
@@ -133,7 +168,10 @@ bb1 = True
 T12 =  0
 while bRunCam:
     if Q.empty():
-        bytes = teensy.readline()
+        try:
+            bytes = teensy.readline()
+        except:
+            break # disconnected?
         for c in bytes.decode():
             Q.put(c)
         continue
@@ -150,10 +188,10 @@ while bRunCam:
         if col == -1: # the first character received should indicate which row
             is_b64, value = b64_decypher(c)
             if not is_b64:
-                print('Unexpected character; not a row number')
+                #print('Unexpected character; not a row number')
                 col = -2 # reset
             elif value < 0 or value > 23:
-                print('Row number out of range')
+                #print('Row number out of range')
                 col = -2 # reset
             else:
                 row = value # we're ready to read the row now
@@ -170,7 +208,7 @@ while bRunCam:
                 if row == 23:
                     update_figure()
             else:
-                print('Unexpected character at end of row')
+                #print('Unexpected character at end of row')
                 col = -2 # something's gone wrong; reset
             continue
         # at this point, we have row=0..23 and col=0..31
@@ -192,8 +230,9 @@ while bRunCam:
 if csv is not None:
     csv.close()
 
-teensy.write(b'auto off;')
-teensy.close()
-
-print("")
-print("Exiting.")
+try:
+    teensy.write(b'auto off;')
+    teensy.close()
+except:
+    if not args.service:
+        print("ircam: device disconnected?")
